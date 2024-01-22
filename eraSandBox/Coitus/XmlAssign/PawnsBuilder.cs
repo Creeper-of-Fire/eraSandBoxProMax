7357953c339125fdea902a47183663082377e6fb
Creeper-of-Fire <2743539045@qq.com>
2024-01-22 20:14:03 +0800

    PawnBuilder

---
+++ eraSandBox/Coitus/XmlAssign/PawnsBuilder.cs
@@ -0,0 +1,19 @@
+﻿namespace eraSandBox.Coitus
+{
+    public class PawnsBuilder
+    {
+        private PawnsBuilder()
+        {
+        }
+        public static PawnsBuilder Instance { get; } = new PawnsBuilder();
+
+        public static TestPawn MakePawn(int heightMillimeter = 1700, string template = "人类")
+        {
+            var pawn = new TestPawn(heightMillimeter, template);
+            pawn.parts.totalParts = PartsBuilder.MakeParts(pawn, pawn.template);
+            pawn.parts.Initialize();
+            pawn.parts.coitusVaginaSystem.UpdateRoutesTotally();
+            return pawn;
+        }
+    }
+}
